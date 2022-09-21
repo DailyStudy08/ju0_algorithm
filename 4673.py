412acdd3f9ed3a41ec84e535215a3a6fd5d6064a
ju0ssafy8 <ssafy8juoh@gmail.com>
2022-09-22 00:23:36 +0900

    4673

---
+++ 4673.py
@@ -0,0 +1,12 @@
+def d(n):
+    numList = list(map(int,str(n))) 
+    num = sum(numList) + n
+    return num
+
+result = [x for x in range(1,10000)]
+for i in range(1,10000):
+    temp = d(i)
+    if temp in result:
+        result.remove(temp)
+for el in result:
+    print(el)
