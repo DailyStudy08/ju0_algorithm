03a5839cf9d9c7bfff08095d56862b709fec567b
ju0ssafy8 <ssafy8juoh@gmail.com>
2022-10-16 18:03:31 +0900

    20221016 안전지대

---
+++ programmers/입문/안전지대.py
@@ -0,0 +1,18 @@
+dr = [0, 0, 1, 1, 1, -1, -1, -1]
+dc = [1, -1, 0, 1, -1, 0, 1, -1]
+
+def solution(board):
+    n = len(board)
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                for d in range(8):
+                    nr, nc = i + dr[d], j + dc[d] 
+                    if 0 <= nr < n and 0 <= nc < n:
+                        if board[nr][nc] == 0:
+                            board[nr][nc] = 2
+                            
+    answer = 0
+    for i in range(n):
+        answer += board[i].count(0)
+    return answers
